92e514f07164af92d98be025036120b68309a7ab
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-07 07:43:04 -0500

    changed button text, changed button style, added surrounding text with table

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const clickMe = document.querySelector('#clickMe');
 
 clickMe.addEventListener('click', e => {
-    alert("Hey there! How are you");
+    alert("Don't tell me the sky's the limit when there are footprints on the moon. -Paul Brandt");
 })
+
